5985bf812a4012e9939bed8080fe878f40a15504
Datallboy <github@datallboy.com>
2022-09-07 15:48:20 -0400

    Fix landing page centering

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import logo from '../public/tech_haven_icon.png';
 const Home: NextPage = () => (
   <section>
     <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
-      <div className='mr-auto place-self-center lg:col-span-7'>
+      <div className='lg:mr-auto place-self-center lg:col-span-7'>
         <h1 className='max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl'>
           Tech Haven
         </h1>
@@ -25,7 +25,7 @@ const Home: NextPage = () => (
           Visit Discord
         </a>
       </div>
-      <div className='lg:mt-0 lg:col-span-5 lg:flex'>
+      <div className='lg:mt-0 place-self-center lg:col-span-5 lg:flex'>
         <Image src={logo} alt='mockup' />
       </div>
     </div>
