65448989edd093116b772023abe50e39b1cd4833
Datallboy <github@datallboy.com>
2023-03-29 19:59:57 -0400

    Default sign in to Authentik

--- components/layout/LoginBtn.tsx
+++ components/layout/LoginBtn.tsx
@@ -2,7 +2,7 @@ import { signIn } from 'next-auth/react';
 
 const LoginBtn = () => (
   <button
-    onClick={() => signIn('keycloak')}
+    onClick={() => signIn('authentik')}
     className='inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-200 hover:text-white'
   >
     Sign in
